902779fac10a6d3cd4cc92c3964196a910451f3a
Oleksii Tsvietnov <me@vorakl.name>
2017-02-24 18:11:01 +0100

    Fix a sub-processes termination

--- tests/exitcodes-test.sh
+++ tests/exitcodes-test.sh
@@ -19,6 +19,6 @@ it_catches_all_exitcodes() {
 it_catches_exitcodes_till_errs() {
     set +e
     output="$(RC_VERBOSE=true RC_WAIT_POLICY=wait_err ./trc -D 'sleep 1; exit 2' -D 'sleep 2; exit 3' -F 'exit 0' -F 'exit 4' exit 5 |   sed -n 's|^.*(exitcode=\([[:digit:]]*\)):.*$|\1|p' | tr '\n' ' ')"
-    test "${output}" = "0 4 "
+    test "${output}" = "0 4 143 143 "
 }
 
