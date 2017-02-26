75799c5feaa686972a5f4e88586307304b1c7362
Oleksii Tsvietnov <me@vorakl.name>
2017-02-26 20:48:14 +0100

    Add a new test

--- tests/exitcodes-test.sh
+++ tests/exitcodes-test.sh
@@ -12,13 +12,19 @@ after() {
 
 it_catches_all_exitcodes() {
     set +e
-    output="$(RC_VERBOSE=true RC_WAIT_POLICY=wait_all ./trc -D 'sleep 1; exit 2' -D 'sleep 2; exit 3' -F 'exit 0' -F 'exit 4' exit 5 |   sed -n 's|^.*(exitcode=\([[:digit:]]*\)):.*$|\1|p' | tr '\n' ' ')"
-    test "${output}" = "0 4 5 2 3 "
+    output="$(RC_VERBOSE=true RC_WAIT_POLICY=wait_all ./trc -H 'exit 0' -D 'sleep 1; exit 2' -D 'sleep 2; exit 3' -F 'exit 0' -F 'exit 4' exit 5 |   sed -n 's|^.*(exitcode=\([[:digit:]]*\)):.*$|\1|p' | tr '\n' ' ')"
+    test "${output}" = "0 4 5 2 3 0 "
 }
 
 it_catches_exitcodes_till_errs() {
     set +e
     output="$(RC_VERBOSE=true RC_WAIT_POLICY=wait_err ./trc -D 'sleep 1; exit 2' -D 'sleep 2; exit 3' -F 'exit 0' -F 'exit 4' exit 5 |   sed -n 's|^.*(exitcode=\([[:digit:]]*\)):.*$|\1|p' | tr '\n' ' ')"
     test "${output}" = "0 4 143 143 "
+}
+
+it_rewrite_exitcode_on_halting() {
+    set +e
+    ./trc -H 'exit 11' exit 5
+    test $? -eq 11
 }
 
