52b3e60724dd9cfb6c99c79b0b00e1e32e778505
Oleksii Tsvietnov <me@vorakl.name>
2017-03-02 21:45:01 +0100

    Add new tests

--- tests/exitcodes-test.sh
+++ tests/exitcodes-test.sh
@@ -16,15 +16,48 @@ it_catches_all_exitcodes() {
     test "${output}" = "0 4 5 2 3 0 "
 }
 
-it_catches_exitcodes_till_errs() {
+it_catches_exitcodes_till_errs_in_sync() {
     set +e
     output="$(RC_VERBOSE=true RC_WAIT_POLICY=wait_err ./trc -D 'sleep 1; exit 2' -D 'sleep 2; exit 3' -F 'exit 0' -F 'exit 4' exit 5 |   sed -n 's|^.*(exitcode=\([[:digit:]]*\)):.*$|\1|p' | tr '\n' ' ')"
     test "${output}" = "0 4 143 143 "
 }
 
-it_rewrite_exitcode_on_halting() {
+it_rewrites_exitcode_on_halting_to_err() {
     set +e
     ./trc -H 'exit 11' exit 5
     test $? -eq 11
+}
+
+it_rewrites_exitcode_on_halting_to_ok() {
+    set +e
+    ./trc -H 'exit 0' exit 5
+    test $? -eq 0
+}
+
+it_fails_on_booting_imm() {
+    set +e
+    ./trc -B 'exit 22' -H 'exit 0' exit 5
+    test $? -eq 22
+}
+
+it_exits_143_on_async_any() {
+    set +e
+    output=$(RC_WAIT_POLICY=wait_any ./trc -D 'echo 11; exit 11' -D 'sleep 1; echo 22; exit 22')
+    test $? -eq 143
+    test "${output}" = "11"
+}
+
+it_exits_last_status_on_sync_any() {
+    set +e
+    output=$(RC_WAIT_POLICY=wait_any ./trc -F 'echo 11; exit 11' -F 'echo 22; exit 22')
+    test $? -eq 11
+    test "${output}" = "11"
+}
+
+it_exits_last_syncstatus_on_mix_any() {
+    set +e
+    output=$(RC_WAIT_POLICY=wait_any ./trc -D 'exit 33' -F 'echo 11; exit 11' -F 'echo 22; exit 22')
+    test $? -eq 11
+    test "${output}" = "11"
 }
 
--- tests/modes-test.sh
+++ tests/modes-test.sh
@@ -18,7 +18,7 @@ it_boot() {
 
 it_async_any() {
     set +e
-    output="$(./trc -D 'echo Hello' -D 'sleep 1; echo World')"
+    output="$(RC_WAIT_POLICY=wait_any ./trc -D 'echo Hello' -D 'sleep 1; echo World')"
     test "${output}" = "Hello"
 }
 
@@ -29,17 +29,23 @@ it_async_all() {
 
 it_sync_any() {
     set +e
-    output="$(./trc -F 'echo Hello' echo World)"
+    output="$(RC_WAIT_POLICY=wait_any ./trc -F 'echo Hello' echo World)"
     test "${output}" = "Hello"
 }
 
 it_sync_all() {
+    set +e
+    output="$(RC_WAIT_POLICY=wait_all ./trc -F 'echo -n "Hello "' -F 'echo World')"
+    test "${output}" = "Hello World"
+}
+
+it_mix_all() {
     output="$(RC_WAIT_POLICY=wait_all ./trc -B 'export wait_sec=1' -D 'sleep ${wait_sec}; echo Hello' -F 'echo -n "World "')"
     test "${output}" = "World Hello"
 }
 
 it_isolated() {
-    output="$(RC_WAIT_POLICY=wait_all ./trc -B 'export v1=A' -D 'export v2=B' -F 'export v3=C' -F 'echo "${v1}${v2}${v3}"')"
-    test "${output}" = "A"
+    output="$(RC_WAIT_POLICY=wait_all ./trc -B 'export v1=A' -H 'echo "${v1}${v2}${v3}"' -D 'export v2=B' -F 'export v3=C' -F 'echo -n "${v1}${v2}${v3}"')"
+    test "${output}" = "AA"
 }
 
